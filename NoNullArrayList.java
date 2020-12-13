40ea7e59b1733292ec4b4277b1a85d45302a6024
Kenli8504 <kli22@stuy.edu>
2020-12-13 17:19:38 -0500

    Put in the constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 
 public class NoNullArrayList <T> extends ArrayList <T> {
 
+  public NoNullArrayList (){
+    super();
+  }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+
   public boolean add(T element){
     if (element == null){
       throw new IllegalArgumentException("You can't have void values in NoNullArrayLists");
